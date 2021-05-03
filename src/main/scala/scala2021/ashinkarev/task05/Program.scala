c138a2ee84779eaaedf5cba10119dbb18d60cfbc
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 18:06:38 +0500

    feat(task05): handle unknown findManagerName case

---
+++ src/main/scala/scala2021/ashinkarev/task05/Program.scala
@@ -0,0 +1,34 @@
+package scala2021.ashinkarev.task05
+
+case class Employee(id: Int, name: String, departmentId: Int)
+case class Department(id: Int, name: String)
+case class Manager(department: String, employeeId: Int)
+case class Info(employee: String, department: String, manager: String)
+
+object Program extends App {
+  val employees = List(
+    Employee(1, "Steve", 1),
+    Employee(3, "Mark", 1),
+    Employee(4, "Jane", 1),
+    Employee(7, "Samuel", 2),
+    Employee(10, "Igor", 2),
+    Employee(11, "Naveen", 4),
+    Employee(12, "Christy", 5),
+    Employee(15, "Megan", 3)
+  )
+  val departments = List(
+    Department(1, "Marketing"),
+    Department(2, "Sales"),
+    Department(3, "Research"),
+    Department(4, "IT"),
+  )
+  val managers = List(
+    Manager("Marketing", 1),
+    Manager("Sales", 10),
+    Manager("IT", 14),
+  )
+
+  def findManagerName(employee: String): Option[String] = {
+    return None
+  }
+}
