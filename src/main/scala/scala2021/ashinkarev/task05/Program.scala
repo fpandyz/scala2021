ac0205c53c6c730516767af8868673e059bc3312
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 19:06:29 +0500

    feat(task05): handle Steve findManagerName case

--- src/main/scala/scala2021/ashinkarev/task05/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task05/Program.scala
@@ -5,8 +5,8 @@ case class Department(id: Int, name: String)
 case class Manager(department: String, employeeId: Int)
 case class Info(employee: String, department: String, manager: String)
 
-object Program extends App {
-  val employees = List(
+class EmployeesProvider {
+  private val employees = List(
     Employee(1, "Steve", 1),
     Employee(3, "Mark", 1),
     Employee(4, "Jane", 1),
@@ -16,19 +16,62 @@ object Program extends App {
     Employee(12, "Christy", 5),
     Employee(15, "Megan", 3)
   )
-  val departments = List(
+
+  def findEmployeeByName(employeeName: String): Option[Employee] = {
+    return employees.find(employee => employee.name == employeeName)
+  }
+
+  def findEmployeeById(employeeId: Int): Option[Employee] = {
+    return employees.find(employee => employee.id == employeeId)
+  }
+}
+
+class DepartmentsProvider {
+  private val departments = List(
     Department(1, "Marketing"),
     Department(2, "Sales"),
     Department(3, "Research"),
     Department(4, "IT"),
   )
-  val managers = List(
+
+  def findEmployeeDepartment(employee: Employee): Option[Department] = {
+    return departments.find(department => department.id == employee.departmentId)
+  }
+}
+
+class ManagersProvider {
+  private val managers = List(
     Manager("Marketing", 1),
     Manager("Sales", 10),
     Manager("IT", 14),
   )
 
-  def findManagerName(employee: String): Option[String] = {
-    return None
+  def findDepartmentManager(department: Department): Option[Manager] = {
+    return managers.find(manager => manager.department == department.name)
+  }
+}
+
+object Program extends App {
+  def findManagerName(employeeName: String): Option[String] = {
+    var employeeByName = new EmployeesProvider().findEmployeeByName(employeeName)
+
+    val manager = employeeByName match {
+      case Some(employee) => findEmployeeManager(employee)
+      case None => None
+    }
+
+    val managerEmployee = manager match {
+      case Some(manager) => new EmployeesProvider().findEmployeeById(manager.employeeId)
+      case None => None
+    }
+
+    return managerEmployee.map(_.name)
+  }
+
+  def findEmployeeManager(employee: Employee): Option[Manager] = {
+    new DepartmentsProvider().findEmployeeDepartment(employee) match {
+      case Some(department) => new ManagersProvider().findDepartmentManager(department)
+      case None => None 
+    }
   }
 }
