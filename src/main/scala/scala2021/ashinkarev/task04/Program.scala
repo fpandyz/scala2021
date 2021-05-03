99bc49ea5165229d445a7e491784647236070885
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 16:21:31 +0500

    feat(task04): handle empty coins case

--- src/main/scala/scala2021/ashinkarev/task04/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task04/Program.scala
@@ -2,10 +2,14 @@ package scala2021.ashinkarev.task04
 
 object Program extends App {
   def canReturnChange(coins: List[Int] , neededChange: Int): Boolean = {
+    if (coins.length == 0) {
+      return false
+    }
+
     if (coins.length == 1) {
       return coins(0) == neededChange
     }
-    
+
     return true
   }
 
