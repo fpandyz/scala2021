af0e306cbf1175e1180b1bc89309a9a9610b0ec2
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 19:58:20 +0500

    ref(task05): use findManagerNameOrError in findManagerName

--- src/main/scala/scala2021/ashinkarev/task05/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task05/Program.scala
@@ -5,12 +5,10 @@ case class Info(employee: String, department: String, manager: String)
 object Program extends App {
   // Найти имя менеджера департамента, в котором работает сотрудник по имени сотрудника
   def findManagerName(employeeName: String): Option[String] = {
-    for {
-      employeeByName <- new EmployeesProvider().findEmployeeByName(employeeName)
-      employeeDepartment <- new DepartmentsProvider().findEmployeeDepartment(employeeByName)
-      departmentManager <- new ManagersProvider().findDepartmentManager(employeeDepartment)
-      managerEmployee <- new EmployeesProvider().findEmployeeById(departmentManager.employeeId)
-    } yield managerEmployee.name
+    findManagerNameOrError(employeeName) match {
+      case Left(value) => None
+      case Right(value) => Some(value)
+    }
   }
    
   // Найти имя менеджера по имени сотрудника, в случае ошибки в данных - указать что именно не так
