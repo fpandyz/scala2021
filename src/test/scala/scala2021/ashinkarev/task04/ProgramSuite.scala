86a7e3464fe433f56b6026c59d85c1a9249c0f46
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 16:20:47 +0500

    test(task04): add empty coins case

--- src/test/scala/scala2021/ashinkarev/task04/ProgramSuite.scala
+++ src/test/scala/scala2021/ashinkarev/task04/ProgramSuite.scala
@@ -10,11 +10,16 @@ class ProgramSuite extends AnyFunSuite with TableDrivenPropertyChecks with Scala
 
   import Program.canReturnChange
 
-  test("single coin list => true for the same coin") {
+  test("empty coins list => false fpor any change") {
+    assert(canReturnChange(List(), 0) == false)
+    assert(canReturnChange(List(), 1) == false)
+  }
+
+  test("single coin list => true for the same change") {
     assert(canReturnChange(List(1), 1))
   }
 
-  test("single coin list => false for a different coin") {
+  test("single coin list => false for a different change") {
     assert(canReturnChange(List(1), 2) == false)
     assert(canReturnChange(List(2), 1) == false)
   }
