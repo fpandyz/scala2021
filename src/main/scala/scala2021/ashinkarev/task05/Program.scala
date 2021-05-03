369387513249dc62509ae4b121e65e3b613ede8b
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-03 19:36:53 +0500

    cleanup(task05): remove redundant code, add comment

--- src/main/scala/scala2021/ashinkarev/task05/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task05/Program.scala
@@ -3,6 +3,7 @@ package scala2021.ashinkarev.task05
 case class Info(employee: String, department: String, manager: String)
 
 object Program extends App {
+  // Найти имя менеджера департамента, в котором работает сотрудник по имени сотрудника
   def findManagerName(employeeName: String): Option[String] = {
     for {
       employeeByName <- new EmployeesProvider().findEmployeeByName(employeeName)
@@ -10,12 +11,5 @@ object Program extends App {
       departmentManager <- new ManagersProvider().findDepartmentManager(employeeDepartment)
       managerEmployee <- new EmployeesProvider().findEmployeeById(departmentManager.employeeId)
     } yield managerEmployee.name
-  }
-
-  def findEmployeeManager(employee: Employee): Option[Manager] = {
-    new DepartmentsProvider().findEmployeeDepartment(employee) match {
-      case Some(department) => new ManagersProvider().findDepartmentManager(department)
-      case None => None 
-    }
   }
 }
