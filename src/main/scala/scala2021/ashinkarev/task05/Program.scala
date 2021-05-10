a6244ecdb2b61636784df18b2152750ada112d58
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-10 15:43:35 +0500

    ref(task05): use cats for findManagerNameOrErrorAsync with EitherT

--- src/main/scala/scala2021/ashinkarev/task05/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task05/Program.scala
@@ -3,6 +3,9 @@ package scala2021.ashinkarev.task05
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 
+import cats._
+import cats.data.EitherT
+
 case class Info(employee: String, department: String, manager: String)
 
 object Program extends App {
@@ -24,29 +27,16 @@ object Program extends App {
     } yield managerEmployee.name
   }
 
-  // ToDo
-  // https://eed3si9n.com/herding-cats/stacking-future-and-either.html
-  // https://stackoverflow.com/questions/47419415/for-comprehensions-future-and-either
-  // https://typelevel.org/cats/datatypes/eithert.html
   // Найти имя менеджера по имени сотрудника, в случае ошибки в данных - указать что именно не так и сделать все это асинхронно
   def findManagerNameOrErrorAsync(employeeName: String): Future[Either[String, String]] = {
-    new EmployeesProvider().findEmployeeByNameOrErrorAsync(employeeName) flatMap {
-        case Right(employeeByName) =>
-          new DepartmentsProvider().findEmployeeDepartmentOrErrorAsync(employeeByName) flatMap {
-            case Right(employeeDepartment) => 
-              new ManagersProvider().findDepartmentManagerOrErrorAsync(employeeDepartment) flatMap {
-                case Right(departmentManager) => 
-                  new EmployeesProvider().findEmployeeByIdOrErrorAsync(departmentManager.employeeId) flatMap {
-                    case Right(managerEmployee) => Future.successful(Right(managerEmployee.name))
-                    case Left(value) => Future.successful(Left(value))
-                  }
-                case Left(value) => Future.successful(Left(value))
-              }
-            case Left(value) =>  Future.successful(Left(value))
-          }
-        case Left(value) =>
-          Future.successful(Left(value))
-      }
+    val result = for {
+      employeeByName <- EitherT(new EmployeesProvider().findEmployeeByNameOrErrorAsync(employeeName))
+      employeeDepartment <- EitherT(new DepartmentsProvider().findEmployeeDepartmentOrErrorAsync(employeeByName))
+      departmentManager <- EitherT(new ManagersProvider().findDepartmentManagerOrErrorAsync(employeeDepartment))
+      managerEmployee <- EitherT(new EmployeesProvider().findEmployeeByIdOrErrorAsync(departmentManager.employeeId))
+    } yield managerEmployee.name;
+
+    return result.value;
   }
 
   def findEmployeeManagers(): List[Info] = {
