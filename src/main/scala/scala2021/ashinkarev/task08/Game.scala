4d38e061ee0a919c9bb6c11de44ebd665b985d30
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-13 18:02:53 +0500

    feat(task08): handle empty string as 0 score and create symbols consts

---
+++ src/main/scala/scala2021/ashinkarev/task08/Game.scala
@@ -0,0 +1,12 @@
+package scala2021.ashinkarev.task08
+
+class Game(input: String) {
+  var STRIKE = 'X';
+  var SPARE = '/';
+  var MISS = '-';
+  var FRAME = '|';
+
+  def calculateScore(): Int = {
+    return 0;
+  }
+}
